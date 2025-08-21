43a4db67033304d162a9efbb1cccc02da1c46bf0
Caio T <caio.trocilo@gmail.com>
2025-08-21 16:15:06 -0300

    Estruturando a agenda

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Container } from "./components/container/container";
+import { Cabecalho } from "./components/cabecalho/cabecalho";
+import { Separador } from "./components/separador/separador";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Container],
+  imports: [RouterOutlet, Container, Cabecalho, Separador],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('indexa');
+  alfabetp: string = 'abcdefghijklmnopqrstuvwxyz'
 }
